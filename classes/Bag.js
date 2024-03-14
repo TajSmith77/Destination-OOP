0d0e0166f5b19c29dbc9c2ff201eebbc53f5c4ad
Taj Smith <TajSmith@Deloitte.com>
2024-03-14 16:14:46 -0400

    Created Bag class and tests

--- classes/Bag.js
+++ classes/Bag.js
@@ -1,6 +1,17 @@
 class Bag {
+    #owner;
+    constructor(weight, id, owner){
+        this.weight = weight;
+        this.id = id;
+        this.#owner = owner || null;
+    }
+    getOwner(){
+        return this.#owner;
+    }
 
-
+    assignOwner(person){
+        this.#owner = person;
+    }
 }
 
 
